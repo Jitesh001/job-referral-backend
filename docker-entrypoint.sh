9836bbd25bbe969d93ea5e04e87674cfd68d38a7
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2025-08-08 13:12:32 +0530

    removed celery

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,5 @@ set -e  # Exit if any command fails
 echo "Running database migrations..."
 python manage.py migrate
 
-echo "Starting Celery worker..."
-celery -A backend.celery_app worker -l info &
-CELERY_WORKER_PID=$!
-
-echo "Starting Celery Beat..."
-celery -A backend.celery_app beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-CELERY_BEAT_PID=$!
-
 echo "Starting Gunicorn..."
 exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
