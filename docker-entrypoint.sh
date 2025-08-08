ae6801ce96960a847e7cfc1ff48e97f8e37adcbe
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2025-08-08 12:40:53 +0530

    script changed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,11 +16,3 @@ python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000 &
-
-echo "Starting Celery worker..."
-celery -A backend.celery_app worker -l info &
-
-# Delay Celery Beat until migrations are applied
-sleep 5
-echo "Starting Celery Beat..."
-celery -A backend.celery_app beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
