e6982859209f8ab035000dc37ae684888f7d3d19
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2025-08-08 13:09:32 +0530

    script changed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command fails
+set -e  # Exit if any command fails
 
-echo "Applying database migrations..."
+echo "Running database migrations..."
 python manage.py migrate
 
 echo "Starting Celery worker..."
 celery -A backend.celery_app worker -l info &
+CELERY_WORKER_PID=$!
 
 echo "Starting Celery Beat..."
 celery -A backend.celery_app beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+CELERY_BEAT_PID=$!
 
 echo "Starting Gunicorn..."
 exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
