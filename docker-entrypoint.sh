bf44d31a538d11f8d2bc5cfa2589b13344898446
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2025-08-08 17:27:42 +0530

    volume mounted

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-set -e  # Exit if any command fails
+# Run migrations
+python manage.py migrate
 
-echo "---- ENV DEBUG ----"
-echo "DATABASE_URL = $DATABASE_URL"
-echo "--------------------"
-
-echo "Starting Gunicorn..."
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+# Start the server
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
