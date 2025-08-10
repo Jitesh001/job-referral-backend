e029e807b2e6f937ebff029abe5d3f1c634c6715
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2025-08-10 13:36:38 +0530

    collectstatic added

--- backend/settings.py
+++ backend/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -63,6 +64,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "backend.urls"
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,8 @@
 echo "Applying database migrations"
 python manage.py migrate
 
+python manage.py collectstatic --noinput
+
 # Start server
 echo "Starting server"
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000 & celery -A backend.celery_app worker -l info & celery -A backend.celery_app beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
