92644e345d76e12ea89985ebe4575f9f515dd46a
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2025-08-12 13:46:00 +0530

    script updated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --noinput
 
 # Start server
 echo "Starting server"
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000 & celery -A backend.celery_app worker -l info & celery -A backend.celery_app beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000 & celery -A backend.celery_app worker -l info --pool=solo --concurrency=2 & celery -A backend.celery_app beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
